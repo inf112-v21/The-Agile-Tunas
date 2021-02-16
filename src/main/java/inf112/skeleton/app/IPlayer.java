7d974c62d072d802dcdd1f0477d2238ecbc35188
Lisa Maria Eliassen <lisaeli@hotmail.no>
2021-02-16 17:08:53 +0100

    Laget noen metoder for IPlayer/Player.

--- src/main/java/inf112/skeleton/app/IPlayer.java
+++ src/main/java/inf112/skeleton/app/IPlayer.java
@@ -1,5 +1,20 @@
 package inf112.skeleton.app;
 
+import java.util.ArrayList;
+
 public interface IPlayer {
-    
+
+    Robot getRobot();
+    // int getRobotID();
+
+    void setProgram(ArrayList<Card> programCards);
+
+    ArrayList<Card> getProgram();
+
+    ArrayList<Card> getDeck();
+
+    void setPowerDown();
+
+    String getName();
+    // int getPlayerID();
 }
--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -1,4 +1,70 @@
 package inf112.skeleton.app;
 
+import java.util.ArrayList;
+
 public class Player implements IPlayer {
+    private Robot robot;
+    //private int robotID;
+    private String name;
+    //private int playerID;
+    //private CardDeck playerDeck;
+    private ArrayList<Card> program;
+
+
+    public Player(Robot robot, String name) {
+        //this.playerDeck = new CardDeck(new ArrayList<>());
+        this.robot = robot;
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Robot getRobot() {
+        return robot;
+    }
+
+    /**
+     *
+     * @param listOfCards
+     */
+    @Override
+    public void setProgram(ArrayList<Card> listOfCards) {
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public ArrayList<Card> getProgram() {
+        return program;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public ArrayList<Card> getDeck() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void setPowerDown() {
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
 }
