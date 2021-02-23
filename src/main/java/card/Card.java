8cc994924315ceb9e58b7c886c468cf81c703db1
Jacob Hagan <jacob.hagan@student.uib.no>
2021-02-23 23:02:45 +0100

    Added test for the Card class

--- src/main/java/card/Card.java
+++ src/main/java/card/Card.java
@@ -11,7 +11,7 @@ public class Card implements ICard {
      * @param type
      * @param priority
      */
-    Card(CardType type, int priority){
+    public Card(CardType type, int priority){
         this.type = type;
         this.priority = priority;
     }
---
+++ src/test/java/inf112/skeleton/app/CardTest.java
@@ -0,0 +1,29 @@
+package inf112.skeleton.app;
+
+import card.Card;
+import card.CardType;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CardTest {
+
+    @Test
+    public void CardHasCorrectType(){
+
+        for (CardType type : CardType.values()){
+            Card card = new Card(type, 1000);
+            assertEquals(type,card.getType());
+        }
+    }
+
+    @Test
+    public void CardReturnsCorrectPriority(){
+        int priority = 1337;
+        Card card = new Card(CardType.MOVE_ONE, priority);
+
+        assertEquals(priority,card.getPriority());
+    }
+
+
+
+}
