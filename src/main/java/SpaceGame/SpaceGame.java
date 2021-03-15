560b1727200401e70621b50c558d79a8634c1ff5
Robin Lange <robinlang@hotmail.com>
2021-03-15 16:38:59 +0100

    Fikset SpaceGame, som vi ikke skal bruke til noe

---
+++ src/main/java/SpaceGame/SpaceGame.java
@@ -0,0 +1,36 @@
+package SpaceGame;
+
+import SpaceGameScreens.MainMenuScreen;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SpaceGame extends Game {
+
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 720;
+
+
+    public SpriteBatch batch;
+
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        //this.setScreen(new GameScreen(this));
+        this.setScreen(new MainMenuScreen(this));
+
+
+
+
+    }
+
+    @Override
+    public void render() {
+        super.render();
+
+
+
+
+
+    }
+}
