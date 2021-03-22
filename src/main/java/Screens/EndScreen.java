7b42e7301d9c8bcf3417a9263964dd52c846c82f
Robin Lange <robinlang@hotmail.com>
2021-03-22 09:09:52 +0100

    Lagt til endringene som ble gjort i code with me av Jacob, Lisa og Olesya

--- src/main/java/Screens/EndScreen.java
+++ src/main/java/Screens/EndScreen.java
@@ -9,4 +9,39 @@ public class EndScreen implements Screen {
     public EndScreen(ScreenOrchestrator screenOrchestrator) {
         parent = screenOrchestrator;
     }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
