302c88f7ea4a7ce6c63fb90e8cf276313403168c
Jacob Hagan <jacob.hagan@student.uib.no>
2021-03-25 23:52:52 +0100

    Fikset test for server

--- src/test/java/tests/ServerTest.java
+++ src/test/java/tests/ServerTest.java
@@ -3,29 +3,58 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import game.MultiplayerGameHandler;
 import network.GameClient;
 import network.GameServer;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class ServerTest {
+public class ServerTest extends ApplicationTest{
+
+    @Before
+    public void initializeTest(){
+        ApplicationTest game = new ApplicationTest();
+    }
     /**
-     * Semi-manual test. Insert host and name in the pop-up windows.
-     * @throws IOException
+     * Test that the clients can connect to the host
+     *
      */
     @Test
-    public void twoClientsConnectToServer() throws IOException, InterruptedException {
-        GameServer server = new GameServer();
-        GameClient client1 = new GameClient("localhost","test", new Stage(), new MultiplayerGameHandler());
-        GameClient client2 = new GameClient("localhost","test", new Stage(), new MultiplayerGameHandler());
+    public void twoClientsConnectToServer() {
+        Gdx.app.postRunnable(() -> {
+            GameServer server = null;
+            try {
+                server = new GameServer();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        //Wait for the server to update
-        TimeUnit.SECONDS.sleep(3);
-        assertEquals(2,server.numberOfPlayers);
+            try {
+                GameClient client1 = new GameClient("localhost","test", new Stage(), new MultiplayerGameHandler());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                GameClient client2 = new GameClient("localhost","test", new Stage(), new MultiplayerGameHandler());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Wait for the server to update
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            assert server != null;
+            assertEquals(2,server.numberOfPlayers);
+        });
+
 
     }
 }
